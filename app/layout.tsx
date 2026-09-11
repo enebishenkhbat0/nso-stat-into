66d743536e7c2f282afd0076e5edc992c5b15ee8
Enebish <enebish@nso.mn>
2026-09-11 11:58:58 +0800

    medkueee

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { ReactNode } from "react";
 import "./globals.css";
 
 export const metadata = {
-  title: "NSO statcate intro dashboards",
+  title: "Үндэсний статистикийн хороо",
 };
 
 export default function RootLayout({ children }: { children: ReactNode }) {
